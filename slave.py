c89d0edac7b3901e1100e167bd0742b4843f66e0
Chapsone <126812939+Chapsone@users.noreply.github.com>
2023-03-15 16:25:18 +0100

    Add files via upload

--- slave.py
+++ slave.py
@@ -7,32 +7,28 @@ import serial
 import time
 
 
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout= 1.0)
+time.sleep(0.5)
+ser.reset_input_buffer()
+
 class controller(Node):
     def __init__(self):
+
         super().__init__("Slave")
-        self.cmd_listnerr_ =self.create_subscription(
-            Twist, "/demo/cmd_vel", self.data_usage,10)
+
+        self.cmd_listnerr_ =self.create_subscription(Twist, "/demo/cmd_vel", self.data_usage,10)
 
     def data_usage(self, msg : Twist):
-
-        ser = serial.Serial('/dev/ttyACM0', 115200, timeout= 1.0)
-        time.sleep(0.5)
-        ser.reset_input_buffer()
 
         vx = msg.linear.x
         vz = msg.linear.z
 
         cmd = str(vx) + 'a' + str(vz) + 'e'
         ser.write(cmd.encode('utf-8'))
-        ser.write("\n".encode('utf-8')) 
+        ser.write("\n".encode('utf-8'))
     
         print(cmd)
-
-
         time.sleep(0.01)
-
-
-
 
 def main(args=None):
     rclpy.init(args=args)
