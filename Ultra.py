cf8fbfbb7214b4860c54ea477e0082b7ed0d043b
Chapsone <126812939+Chapsone@users.noreply.github.com>
2023-03-20 12:26:10 +0000

    Add files via upload

--- Ultra.py
+++ Ultra.py
@@ -12,23 +12,24 @@ GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 GPIO.output(TRIG, False)
-print("waitting for 2s ")
-time.sleep(2)
+print("waitting for 0.5s ")
+time.sleep(0.5)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+while True:
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO) == 0:
-    pulse_start = time.time()
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
 
-while GPIO.input(ECHO) == 1:
-    pulse_end = time.time()
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
 
-pulse_duration = pulse_end - pulse_start 
-distance = pulse_duration * 17150
-distance = round( distance, 2)
-print ("la distance est: " + str(distance))
+    pulse_duration = pulse_end - pulse_start 
+    distance = pulse_duration * 17150
+    distance = round( distance, 2)
+    print ("la distance est: " + str(distance))
 
 GPIO.cleanup()
 
